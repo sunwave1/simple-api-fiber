78cf1f7a69bf1f42fffd8381d9c503c7e766875b
sunwave1 <fellipeanthony1@hotmail.com>
2024-10-17 23:15:14 -0300

    feat: initialize routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"api/internal/route"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 
 	app := fiber.New()
+
+	route.InitializeRoutes(app)
 
 	app.Listen(":8000")
 }
---
+++ internal/route/route.go
@@ -0,0 +1,7 @@
+package route
+
+import "github.com/gofiber/fiber/v2"
+
+func InitializeRoutes(app *fiber.App) {
+
+}
